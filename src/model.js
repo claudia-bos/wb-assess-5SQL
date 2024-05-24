efe8d772a97d8ee20296ecc25a8c7016b4ff5994
Claudia Delgado <claudiadelgado@Claudias-Air.localdomain>
2024-05-24 15:43:37 -0600

    completed queries

--- src/model.js
+++ src/model.js
@@ -1,9 +1,11 @@
-import { DataTypes, Model } from 'sequelize';
+import sequelize, { DataTypes, Model } from 'sequelize';
 import util from 'util';
 import connectToDB from './db.js';
+import { type } from 'os';
 
 const db = await connectToDB('postgresql:///animals');
 
+//This tells sequelize this is a table we will have
 export class Human extends Model {
   [util.inspect.custom]() {
     return this.toJSON();
@@ -11,11 +13,43 @@ export class Human extends Model {
 
   getFullName() {
     // TODO: Implement this method
+    return `${this.fname} ${this.lname}`
   }
 }
 
-// TODO: Human.init()
+        // TODO: Human.init()
+//Actually initializes the data in our table, i can also say that creates the table
+// init takes two object arguments ({}, {})
+// First is the table setup, columns and their datatypes, etc.
+// Second is altering default sequelize configuration
 
+Human.init(
+{
+  humanId: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false
+  },
+  fName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, 
+{
+  modelName: 'human',
+  sequelize: db,
+},
+);
+
+ 
 export class Animal extends Model {
   [util.inspect.custom]() {
     return this.toJSON();
@@ -23,7 +57,35 @@ export class Animal extends Model {
 }
 
 // TODO: Animal.init()
+Animal.init(
+  {
+    animalId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    species: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    birthYear: {
+      type: DataTypes.INTEGER
+    }
+  }, 
+  {
+    modelName: 'animal',
+    sequelize: db, //connects this model with the import sequelize instance
+  },
+);
 
 // TODO: Define Relationship
 
+Human.hasMany(Animal, { foreignKey: 'human_id'});
+Animal.belongsTo(Human, { foreignKey: 'human_id'});
+
+// export { Human, Animal };
 export default db;
--- src/queries.js
+++ src/queries.js
@@ -4,31 +4,58 @@ import { Animal, Human } from './model.js';
 // Get the human with the primary key 2
 export const query1 = null;
 
+const humanwithKey = await Human.findByPk(2)
+return humanwithKey;
+
 // Get the first animal whose species is "fish"
 export const query2 = null;
+
+const animal = await Animal.findOne({ where: { species: 'fish' } })
+return animal
 
 // Get all animals belonging to the human with primary key 5
 export const query3 = null;
 
+const getAllanimal = await Animal.findAll({ where: { human_id: 5 } })
+return getAllanimal
+
 // Get all animals born in a year greater than (but not equal to) 2015.
 export const query4 = null;
+
+const getanimalBornDate = await Animal.findAll({ where: { birth_year: {[Op.gt]: 2015} } })
+return getanimalBornDate
 
 // Get all the humans with first names that start with "J"
 export const query5 = null;
 
+const allHumans = await Human.findAll({ where: { fname: {[Op.startsWith]: 'J'} } })
+return allHumans
+
 // Get all the animals who don't have a birth year
 export const query6 = null;
+
+const animalWithNoBirthDate = await Animal.findAll({ where: { birth_year: false} })
+return animalWithNoBirthDate
 
 // Get all the animals with species "fish" OR "rabbit"
 export const query7 = null;
 
+const species = await Animal.findAll({ where: {[Op.or]: [{ species: 'fish' }, { species: 'rabbit' }] } })
+return species
+
 // Get all the humans who DON'T have an email address that contains "gmail"
 export const query8 = null;
+
+const noGmail = await Human.findAll({ where: { email: {[Op.notILike]: '%gmail%'} } })
+return noGmail
 
 // Continue reading the instructions before you move on!
 
 // Print a directory of humans and their animals
-export async function printHumansAndAnimals() {}
+export async function printHumansAndAnimals() {
+    const humans = await Human.findAll({ include: Animal })
+    
+}
 
 // Return a Set containing the full names of all humans
 // with animals of the given species.
